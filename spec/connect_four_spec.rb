b5dbbe0869bddc65cd60d148f3b5df86721193d4
kenyounot123 <kenlu519@gmail.com>
2023-08-16 23:06:48 -0400

    update #initialize rspec testing to correct output

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,8 +1,7 @@
 require_relative '../lib/connect_four'
 
 describe Game do
-  describe '#place_disk' do 
-  end
+  
 end
 
 #Testing for the Board class will just check if updating the board / winning is correct
@@ -10,16 +9,17 @@ describe Board do
   subject(:board) { described_class.new }
   let(:white_circle) { described_class.new.white_circle }
   let(:black_circle) { described_class.new.black_circle }
+  let(:empty_circle) { described_class.new.empty_circle}
 
   describe '#initialize' do 
     it 'properly initializes the 6 by 7 grid board' do
       empty_board = [
-        [" ", " ", " ", " ", " ", " ", " "],
-        [" ", " ", " ", " ", " ", " ", " "],
-        [" ", " ", " ", " ", " ", " ", " "],
-        [" ", " ", " ", " ", " ", " ", " "],
-        [" ", " ", " ", " ", " ", " ", " "],
-        [" ", " ", " ", " ", " ", " ", " "]
+        [ empty_circle ] * 7,
+        [ empty_circle ] * 7,
+        [ empty_circle ] * 7,
+        [ empty_circle ] * 7,
+        [ empty_circle ] * 7,
+        [ empty_circle ] * 7
       ]
       current_board = board.instance_variable_get(:@grid)
       expect(current_board).to eq(empty_board)
